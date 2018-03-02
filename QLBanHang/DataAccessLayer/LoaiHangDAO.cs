c0a3d5952ecb829bf15f2703d7d0c7bb4ef64dc9
thangtcnb <thangtcnb@gmail.com>
2018-03-02 19:30:55 +0700

    --Mod

--- QLBanHang/DataAccessLayer/LoaiHangDAO.cs
+++ QLBanHang/DataAccessLayer/LoaiHangDAO.cs
@@ -1,38 +1,59 @@
 ﻿//Lớp này dùng để thao tác với bảng LoaiHang
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 //Các thư viện thêm vào
 using System.Data;
 using System.Data.SqlClient;
 using ValueObject;
+
 namespace DataAccessLayer
 {
-    class LoaiHangDAO
+    internal class LoaiHangDAO
     {
-        dbConnect db = new dbConnect();
+        private dbConnect db = new dbConnect();
+
         public DataTable GetData()
         {
             DataTable dt = new DataTable();
             dt = db.GetData("sp_LoaiHang_SelectAll", null);//null là tham số truyền vào
             return dt;
         }
+
         /*
          public DataTable GetData()
          {
             return db.GetData("sp_LoaiHang_SelectAll", null);
          }
          * */
-         public DataTable GetDaTaByID(string ID)
+
+        public DataTable GetDaTaByID(string ID)
         {
             SqlParameter[] para =
             {
                 new SqlParameter("IDLoaiHang", ID)
             };
             return db.GetData("sp_LoaiHang_Select_ByID", para);
+        }
+
+        public int Insert(LoaiHang obj)
+        {
+            SqlParameter[] para =
+             {
+                new SqlParameter("IDLoaiHang", obj.IDLoaiHang),
+                new SqlParameter("TenLoaiHang", obj.TenLoai),
+                new SqlParameter("MoTa", obj.Mota)
+             };
+            return db.ExcuteSQL("sp_LoaiHang_Insert", para);
+        }
+
+        public int Update(LoaiHang obj)
+        {
+            SqlParameter[] para =
+             {
+                new SqlParameter("IDLoaiHang", obj.IDLoaiHang),
+                new SqlParameter("TenLoaiHang", obj.TenLoai),
+                new SqlParameter("MoTa", obj.Mota)
+             };
+            return db.ExcuteSQL("sp_LoaiHang_Insert", para);
         }
     }
 }
